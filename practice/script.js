6e645f753869286c2f29787c6b62d134d139dffe
EC2 Default User <specialized.19991129@gmail.com>
2023-03-27 16:11:21 +0900

    [Progate]JSオブジェクトの復習

--- practice/script.js
+++ practice/script.js
@@ -1,11 +1,14 @@
-$(function() {
-  // 「#language-wrapper」にhoverしたときのhoverイベントを作成してください
-  $('#language-wrapper').hover(
-    function(){
-      $('.language-text').fadeIn();
-    },
-    function(){
-      $('.language-text').fadeOut();
-    }
-  );
-});
+// 定数animalを定義してください
+const animal = {
+  name: "レオ",
+  age: 3,
+  greet: () => {
+    console.log("こんにちは");
+  }
+}
+
+// animalのnameプロパティの値を出力してください
+console.log(animal.name);
+
+// animalのgreetプロパティの関数を実行してください
+animal.greet();
